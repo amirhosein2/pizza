2a19939b747939b75c1cc9cc3b2aa47a4fe14a64
amirhosein2 <sosin@debian>
2015-03-14 16:49:38 -0700

    finish project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
-  def new
+  def index
   end
+  def show
+    @user = User.find(params[:id])
+  end
+  def new
+        @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome, your " << @user.faworit_food << " is ready !"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation, :faworit_food)
+    end
 end
